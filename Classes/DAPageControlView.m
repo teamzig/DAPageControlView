89912b4a759dac8fd6ce64b2eaa64f24b2a4fba2
brookso <boakley@Brookss-iMac.local>
2017-04-07 11:57:42 -0500

    updates for Zig indicator images and XCode 8.3

--- Classes/DAPageControlView.m
+++ Classes/DAPageControlView.m
@@ -11,7 +11,7 @@
 #import "DAPageIndicatorViewCell.h"
 
 
-static NSUInteger const FCMaximumIndicatorsCount = 21;
+static NSUInteger const FCMaximumIndicatorsCount = 7;
 static CGFloat const FCMaximumIndicatorViewWidth = 14.;
 
 
@@ -35,40 +35,28 @@ static CGFloat const FCMaximumIndicatorViewWidth = 14.;
 {
     self = [super initWithFrame:frame];
     if (self) {
-        [self setup];
+        /// Defaults
+        self.numberOfPagesAllowingPerspective = 3;
+        
+        UICollectionViewFlowLayout *collectionViewLayout = [[UICollectionViewFlowLayout alloc] init];
+        collectionViewLayout.scrollDirection = UICollectionViewScrollDirectionHorizontal;
+        collectionViewLayout.minimumInteritemSpacing = collectionViewLayout.minimumLineSpacing = 0.;
+        self.indicatorsView = [[UICollectionView alloc] initWithFrame:self.bounds collectionViewLayout:collectionViewLayout];
+        [self.indicatorsView registerClass:[DAPageIndicatorViewCell class] forCellWithReuseIdentifier:DAPageIndicatorViewCellIdentifier];
+        self.indicatorsView.backgroundColor = [UIColor clearColor];
+        self.indicatorsView.showsHorizontalScrollIndicator = NO;
+        self.indicatorsView.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
+        self.indicatorsView.dataSource = self;
+        self.indicatorsView.delegate = self;
+        self.indicatorsView.scrollEnabled = NO;
+        self.indicatorsView.backgroundColor = [UIColor clearColor];
+        [self addSubview:self.indicatorsView];
+        
+        [self.indicatorsView addObserver:self forKeyPath:@"contentOffset" options:0 context:nil];
+        self.currentPage = 0;
     }
     
     return self;
-}
-
-- (void)awakeFromNib {
-    [self setup];
-}
-
-- (void)setup {
-    /// Defaults
-    self.numberOfPagesAllowingPerspective = 3;
-    
-    UICollectionViewFlowLayout *collectionViewLayout = [[UICollectionViewFlowLayout alloc] init];
-    collectionViewLayout.scrollDirection = UICollectionViewScrollDirectionHorizontal;
-    collectionViewLayout.minimumInteritemSpacing = collectionViewLayout.minimumLineSpacing = 0.;
-    self.indicatorsView = [[UICollectionView alloc] initWithFrame:self.bounds collectionViewLayout:collectionViewLayout];
-    [self.indicatorsView registerClass:[DAPageIndicatorViewCell class] forCellWithReuseIdentifier:DAPageIndicatorViewCellIdentifier];
-    self.indicatorsView.backgroundColor = [UIColor clearColor];
-    self.indicatorsView.showsHorizontalScrollIndicator = NO;
-    self.indicatorsView.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
-    self.indicatorsView.dataSource = self;
-    self.indicatorsView.delegate = self;
-    self.indicatorsView.scrollEnabled = NO;
-    self.indicatorsView.backgroundColor = [UIColor clearColor];
-    [self addSubview:self.indicatorsView];
-    
-    [self.indicatorsView addObserver:self forKeyPath:@"contentOffset" options:0 context:nil];
-    self.currentPage = 0;
-    
-    // default color
-    self.normalColor = [UIColor lightGrayColor];
-    self.selectedColor = [UIColor whiteColor];
 }
 
 - (void)dealloc
@@ -77,19 +65,6 @@ static CGFloat const FCMaximumIndicatorViewWidth = 14.;
 }
 
 #pragma mark - Public
-
-- (void)gotoPage:(NSUInteger)page
-{
-    CGFloat currentIndex = page;
-    if ( page == self.currentPage )
-        return;
-    
-    self.currentPage = page;
-    
-    CGFloat x = self.indicatorsView.contentSize.width * (currentIndex - floorf(0.5 * CGRectGetWidth(self.indicatorsView.frame) / [self indicatorViewWidth])) / self.numberOfPages;
-    x = MIN((self.indicatorsView.contentSize.width - CGRectGetWidth(self.indicatorsView.frame)), MAX(0, x));
-    self.indicatorsView.contentOffset = CGPointMake(x, 0.);
-}
 
 - (void)updateForScrollViewContentOffset:(CGFloat)contentOffset pageSize:(CGFloat)pageSize
 {
@@ -151,13 +126,13 @@ static CGFloat const FCMaximumIndicatorViewWidth = 14.;
     if (index * [self indicatorViewWidth] < offset + self.numberOfPagesAllowingPerspective * [self indicatorViewWidth]) {
         if (offset > 0) {
             CGFloat delta = index * [self indicatorViewWidth] - (offset + self.numberOfPagesAllowingPerspective * [self indicatorViewWidth]);
-            scale = 1 - 0.6 * (fabsf(delta)) / (self.numberOfPagesAllowingPerspective * [self indicatorViewWidth]);
+            scale = 1 - 0.6 * (fabs(delta)) / (self.numberOfPagesAllowingPerspective * [self indicatorViewWidth]);
         }
     } else {
         if ((index + 1) * [self indicatorViewWidth] > offset + CGRectGetWidth(self.indicatorsView.frame) - (self.numberOfPagesAllowingPerspective * [self indicatorViewWidth])) {
             if (offset + CGRectGetWidth(self.indicatorsView.frame) < self.indicatorsView.contentSize.width) {
                 CGFloat delta = (offset + CGRectGetWidth(self.indicatorsView.frame) - (self.numberOfPagesAllowingPerspective * [self indicatorViewWidth])) - (index + 1) * [self indicatorViewWidth];
-                scale = 1 - 0.6 * (fabsf(delta)) / (self.numberOfPagesAllowingPerspective * [self indicatorViewWidth]);
+                scale = 1 - 0.6 * (fabs(delta)) / (self.numberOfPagesAllowingPerspective * [self indicatorViewWidth]);
             }
         }
     }
@@ -257,8 +232,6 @@ static CGFloat const FCMaximumIndicatorViewWidth = 14.;
     DAPageIndicatorViewCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:DAPageIndicatorViewCellIdentifier forIndexPath:indexPath];
     cell.tag = indexPath.row;
     cell.pageIndicatorView.selected = (indexPath.row == self.currentPage);
-    cell.normalColor = self.normalColor;
-    cell.selectedColor = self.selectedColor;
     CGFloat scale = [self scaleForIndicatorAtIndex:indexPath.row];
     cell.pageIndicatorView.transform = CGAffineTransformMakeScale(scale, scale);
     if (indexPath.row == self.numberOfPages - 1) {
--- Classes/DAPageIndicatorViewCell.m
+++ Classes/DAPageIndicatorViewCell.m
@@ -8,7 +8,6 @@
 
 #import "DAPageIndicatorViewCell.h"
 
-
 NSString *const DAPageIndicatorViewCellIdentifier = @"DAPageIndicatorViewCell";
 CGFloat const DAPageIndicatorViewWidth = 7.;
 CGFloat const DAPageIndicatorViewHeight = DAPageIndicatorViewWidth;
@@ -32,43 +31,23 @@ CGFloat const DAPageIndicatorViewHeight = DAPageIndicatorViewWidth;
 {
     self = [super initWithFrame:frame];
     if (self) {
+        NSBundle *bundle = [NSBundle bundleForClass:[self class]];
+        UIImage *indicatorImage = [UIImage imageNamed:@"PageIndicator" inBundle:bundle compatibleWithTraitCollection:nil];
+        UIImage *currentImage = [UIImage imageNamed:@"ActivePageIndicator" inBundle:bundle compatibleWithTraitCollection:nil];
+        
         self.pageIndicatorView = [[UIButton alloc] initWithFrame:CGRectMake(0., 0., DAPageIndicatorViewWidth, DAPageIndicatorViewHeight)];
-        
-        // default
-        UIImage* normalImage = [self imageByDrawingCircleWithColor:[UIColor lightGrayColor] withSize:self.pageIndicatorView.bounds.size];
-        UIImage* selectImage = [self imageByDrawingCircleWithColor:[UIColor whiteColor] withSize:self.pageIndicatorView.bounds.size];
-        [self.pageIndicatorView setBackgroundImage:normalImage forState:UIControlStateNormal];
-        [self.pageIndicatorView setBackgroundImage:selectImage forState:UIControlStateSelected];
-        
-//        [self.pageIndicatorView setBackgroundImage:[UIImage imageNamed:@"DAPageIndicator"] forState:UIControlStateNormal];
-//        [self.pageIndicatorView setBackgroundImage:[UIImage imageNamed:@"DAPageIndicatorCurrent"] forState:UIControlStateSelected];
-
+        self.pageIndicatorView.layer.masksToBounds = YES;
+        self.pageIndicatorView.layer.cornerRadius = self.pageIndicatorView.frame.size.width / 2;
+        [self.pageIndicatorView setBackgroundImage:indicatorImage forState:UIControlStateNormal];
+        [self.pageIndicatorView setBackgroundImage:currentImage forState:UIControlStateSelected];
         self.pageIndicatorView.center = CGPointMake(0.5 * CGRectGetWidth(frame), 0.5 * CGRectGetHeight(frame));
         self.pageIndicatorView.autoresizingMask = UIViewAutoresizingFlexibleLeftMargin | UIViewAutoresizingFlexibleRightMargin | UIViewAutoresizingFlexibleTopMargin | UIViewAutoresizingFlexibleBottomMargin;
         self.pageIndicatorView.userInteractionEnabled = NO;
         [self.contentView addSubview:self.pageIndicatorView];
+        //self.pageIndicatorView.backgroundColor = [UIColor lightGrayColor];
     }
     
     return self;
-}
-
--(void)setNormalColor:(UIColor *)normalColor {
-    
-    if ( _normalColor != normalColor )
-    {
-        UIImage* normalImage = [self imageByDrawingCircleWithColor:normalColor withSize:self.pageIndicatorView.bounds.size];
-        [self.pageIndicatorView setBackgroundImage:normalImage forState:UIControlStateNormal];
-        _normalColor = normalColor;
-    }
-}
-
--(void)setSelectedColor:(UIColor *)selectedColor {
-    
-    if ( _selectedColor != selectedColor ) {
-        UIImage* selectImage = [self imageByDrawingCircleWithColor:selectedColor withSize:self.pageIndicatorView.bounds.size];
-        [self.pageIndicatorView setBackgroundImage:selectImage forState:UIControlStateSelected];
-        _selectedColor = selectedColor;
-    }
 }
 
 - (void)prepareForReuse
@@ -77,23 +56,6 @@ CGFloat const DAPageIndicatorViewHeight = DAPageIndicatorViewWidth;
     self.pageIndicatorView.transform = CGAffineTransformIdentity;
     self.alpha = 1.;
     [self.layer removeAllAnimations];
-}
-
--(UIImage *)imageByDrawingCircleWithColor:(UIColor *)color withSize:(CGSize)size {
-    
-    UIImage* img = nil;
-    UIGraphicsBeginImageContextWithOptions(size, NO, 0.0f);
-    CGContextRef ctx = UIGraphicsGetCurrentContext();
-    CGContextSaveGState(ctx);
-    
-    CGRect rect = CGRectMake(0, 0, size.width, size.height);
-    CGContextSetFillColorWithColor(ctx, color.CGColor);
-    CGContextFillEllipseInRect(ctx, rect);
-    
-    CGContextRestoreGState(ctx);
-    img = UIGraphicsGetImageFromCurrentImageContext();
-    UIGraphicsEndImageContext();
-    return img;
 }
 
 @end
